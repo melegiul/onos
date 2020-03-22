e35c9895abeefef566b8b57288867ee453df47ec
Giuliano Mele <giuliano.mele@student.uni-augsburg.de>
2020-03-22 11:16:05 +0100

    minor changes

--- core/net/src/main/java/org/onosproject/net/intent/impl/compiler/FirewallIntentCompiler.java
+++ core/net/src/main/java/org/onosproject/net/intent/impl/compiler/FirewallIntentCompiler.java
@@ -46,64 +46,6 @@ public class FirewallIntentCompiler
     public List<Intent> compile(FirewallIntent intent, List<Intent> installable) {
         Map<DeviceId, Link> links = new HashMap<>();
 
-        /*final boolean allowMissingPaths = intentAllowsPartialFailure(intent);
-        boolean hasPaths = false;
-        boolean missingSomePaths = false;*/
-
-        /*for (ConnectPoint ingressPoint : intent.ingressPoints()) {
-            *//*if (ingressPoint.deviceId().equals(egressPoint.deviceId())) {
-                if (deviceService.isAvailable(ingressPoint.deviceId())) {
-                    hasPaths = true;
-                } else {
-                    missingSomePaths = true;
-                }
-                continue;
-            }*//*
-
-//            Path path = getPath(intent, ingressPoint.deviceId(), egressPoint.deviceId());
-
-            if (path != null) {
-                hasPaths = true;
-
-                for (Link link : path.links()) {
-                    if (links.containsKey(link.dst().deviceId())) {
-                        // We've already reached the existing tree with the first
-                        // part of this path. Add the merging point with different
-                        // incoming port, but don't add the remainder of the path
-                        // in case it differs from the path we already have.
-                        links.put(link.src().deviceId(), link);
-                        break;
-                    }
-                    links.put(link.src().deviceId(), link);
-                }
-            } else {
-                missingSomePaths = true;
-            }
-        }*/
-
-        // Allocate bandwidth on existing paths if a bandwidth constraint is set
-        /*List<ConnectPoint> ingressCPs =
-                intent.filteredIngressPoints().stream()
-                        .map(fcp -> fcp.connectPoint())
-                        .collect(Collectors.toList());*/
-//        ConnectPoint egressCP = intent.filteredEgressPoint().connectPoint();
-
-        /*List<ConnectPoint> pathCPs =
-                links.values().stream()
-                        .flatMap(l -> Stream.of(l.src(), l.dst()))
-                        .collect(Collectors.toList());
-
-        pathCPs.addAll(ingressCPs);
-        pathCPs.add(egressCP);
-
-        allocateBandwidth(intent, pathCPs);
-
-        if (!hasPaths) {
-            throw new IntentException("Cannot find any path between ingress and egress points.");
-        } else if (!allowMissingPaths && missingSomePaths) {
-            throw new IntentException("Missing some paths between ingress and egress points.");
-        }*/
-
         Intent result = LinkCollectionIntent.builder()
                 .appId(intent.appId())
                 .key(intent.key())
